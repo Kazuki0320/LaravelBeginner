1494c8b9ff0549f05a8a994c17d376dec5d78665
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-01-02 20:12:07 +0900

    ルートパラメータの追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,8 +6,27 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function Hello()
+    public function Hello($id='noname', $pass='unknow')
     {
-        echo "Hello";
+    return <<< EOF
+	<html>
+		<head>
+		<title>Hello</title>
+		<style>
+			body {font-size:16pt; color:#9999; }
+			h1 {font-size:100pt; text-aligin:right, Bolor:#eee;
+			margin:-40px 0px -50px 0px; }
+		</style>
+		</head>
+	<body>
+		<h1>Hello</h1>
+		<p>これは、Helloコントローラのindexです</p>
+		<ul>
+			<li>ID: {$id}</li>
+			<li>PASS: {$pass}</li>
+		</ul>
+	</body>
+	</html>
+	EOF;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use App\Http\Controllers\HelloController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', [HelloController::class, 'Hello']);
+Route::get('hello/{id?}/{pass?}', [HelloController::class, 'Hello']);
