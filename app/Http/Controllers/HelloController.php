ad70b2ac2146db6dd412d2da8aeb2fb792f51e2d
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-01-07 15:05:08 +0900

    サービスクラス作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,33 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-	public function hello(Request $request, Response $response)
-	{
-		$html = <<< EOF
-		<html>
-			<head>
-			<title>Hello</title>
-			<style>
-			body {font-size:16pt; color:#999; }
-			h1 {font-size:120pt; text-align:right; color:#fafafa;
-			margin:-50px 0px -120px 0px;
-			}
-			</style>
-			</head>
-			<body>
-			<h1>Hello</h1>
-			<h3>Request</h3>
-			<pre>{$request}</pre>
-			<h3>Response</h3>
-			<pre>{$response}</pre>
-			</body>
-		</html>
-		EOF;
-			$response->setContent($html);
-			return $response;
+	public function __invoke() {
+		return <<< EOF
+	<html>
+		<head>
+		<title>Hello</title>
+		<style>
+		body {font-size:16pt; color:#999;}
+		h1 {font-size:30pt; text-align:right; color:#eee;
+		margin:-15pt; 0pt 0pt 0pt; }
+		}
+		</style>
+		</head>
+		<body>
+		<h1>single Action</h1>
+		<p>これは、シングルアクションコントローラー</p>
+		</body>
+	</html>
+	EOF;
 	}
 }
