8ed16dfeb1fa1394e3d7f013f752320a911a1a82
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-01-07 16:52:51 +0900

    インスタンス化省略

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,10 +7,17 @@ use App\Services\Test;
 
 class TestController extends Controller
 {
-	public function test()
-	{	
-	$test = new Test;//サービスクラスをインスタンス化する
-	$user = $test->getTestUser();//関数を呼ぶ
-	dd($user);//test
+#	newでサービスクラスを呼びだす
+#	public function test()
+#	{	
+#	$test = new Test;//サービスクラスをインスタンス化する
+#	$user = $test->getTestUser();//関数を呼ぶ
+#	dd($user);//test
+#	}
+#	インスタンス化の省略形		
+	public function test(Test $test)//ここでインスタンス化できる
+	{
+		$user = $test->getTestUser(); //関数を呼ぶ
+		dd($user);
 	}
 }
