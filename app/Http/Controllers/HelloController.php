497fe0d31709dae725461465761baf408e9d07b5
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-01-13 22:50:49 +0900

    サービスクラス用メソッドコメントアウト

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,25 +6,25 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-	public function __invoke() {
-		return <<< EOF
-	<html>
-		<head>
-		<title>Hello</title>
-		<style>
-		body {font-size:16pt; color:#999;}
-		h1 {font-size:30pt; text-align:right; color:#eee;
-		margin:-15pt; 0pt 0pt 0pt; }
-		}
-		</style>
-		</head>
-		<body>
-		<h1>single Action</h1>
-		<p>これは、シングルアクションコントローラー</p>
-		</body>
-	</html>
-	EOF;
-	}
+	// public function __invoke() {
+	// 	return <<< EOF
+	// <html>
+	// 	<head>
+	// 	<title>Hello</title>
+	// 	<style>
+	// 	body {font-size:16pt; color:#999;}
+	// 	h1 {font-size:30pt; text-align:right; color:#eee;
+	// 	margin:-15pt; 0pt 0pt 0pt; }
+	// 	}
+	// 	</style>
+	// 	</head>
+	// 	<body>
+	// 	<h1>single Action</h1>
+	// 	<p>これは、シングルアクションコントローラー</p>
+	// 	</body>
+	// </html>
+	// EOF;
+	// }
 	public function index()
 	{
         $data = ['msg' => 'これはコントローラーから渡されたメッセージです。'];
