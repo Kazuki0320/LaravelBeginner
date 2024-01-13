04628b266cc7406ee435e3d4e65ddcb941809774
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-01-13 22:43:20 +0900

    インデント修正

--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -1,9 +1,9 @@
 <html >
-  <head>
+    <head>
     <title>@yield('title')</title>
     <style>
     body{
-      font-size:16pt; color:#999; margin:5px;
+        font-size:16pt; color:#999; margin:5px;
     }
     th{
         background-color: #999;
@@ -16,50 +16,50 @@
         padding:5px 10px;
     }
     h1{
-      font-size:50pt;
-      text-align:right;
-      color:#f6f6f6f6;
-      margin:-20px 0px -30px 0px;
-      letter-spacing:-4pt;
+        font-size:50pt;
+        text-align:right;
+        color:#f6f6f6f6;
+        margin:-20px 0px -30px 0px;
+        letter-spacing:-4pt;
     }
     ul{
-      font-size:12pt;
+        font-size:12pt;
     }
     hr{
-      margin:25px 100px;
-      border-top:1px dashed #ddd;
+        margin:25px 100px;
+        border-top:1px dashed #ddd;
     }
-      .menutitle{
+    .menutitle{
         font-size:14pt;
         font-weight:bold;
         margin:0px;
-      }
-      .content{
+    }
+    .content{
         margin:10px;
-      }
-      .footer{
+    }
+    .footer{
         text-align:right;
         font-size:10pt;
         margin:10px;
         border-bottom:solid 1px #ccc;
         color:#ccc
-      }
+    }
 
     </style>
-  </head>
-  <body>
+    </head>
+<body>
     <h1>@yield('title')</h1>
     @section('menubar')
     <h2 class="menutitle">メニュー</h2>
     <ul>
-      <li>@show</li>
+        <li>@show</li>
     </ul>
     <hr size="1">
     <div class="content">
-      @yield('content')
+        @yield('content')
     </div>
     <div class="footer">
-      @yield('footer')
+        @yield('footer')
     </div>
-  </body>
+</body>
 </html>
